21e8f7ddb01ca87990ec81f81aa7a66e30db460f
PavanBashetty <pavankumar.rb25@gmail.com>
2023-05-25 14:06:36 +0200

    git test file

---
+++ src/frontend/src/components/userDetailsPage.vue
@@ -0,0 +1,14 @@
+<template>
+<p>User Details</p>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+    name:'userDetailPage'
+})
+</script>
+
+<style>
+</style>
