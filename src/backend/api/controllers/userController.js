edae8fab34765a0693f3cba2cfd04bbee0786734
skandswami <59348205+skandswami@users.noreply.github.com>
2023-06-04 21:38:43 +0200

    implemented add user bets

--- src/backend/api/controllers/userController.js
+++ src/backend/api/controllers/userController.js
@@ -1,51 +1,51 @@
-var express = require('express');
-const jwt = require('jsonwebtoken')
-const User = require('../model/userModel'); 
+var express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 var router = express.Router();
 
 //Create a user at SIGN UP
 const createUser = async (req, res) => {
-    const { id, username, password, email, fullname, dob, balance } = req.body;
-    try {
-      let user = await User.findOne({
-        email
+  const { id, username, password, email, fullname, dob, balance } = req.body;
+  try {
+    let user = await User.findOne({
+      email,
+    });
+    if (!user) {
+      user = await User.create({
+        id,
+        username,
+        password,
+        email,
+        fullname,
+        dob,
+        balance,
       });
-      if (!user) {
-        user = await User.create({
-            id, 
-            username,
-            password, 
-            email, 
-            fullname, 
-            dob, 
-            balance,
-        })
-      }
-      console.log("logged in")
-      const token = constructJwtToken(user)
-      res.status(200).json({ token: token })
-    } catch (error) {
-      console.log(error)
-      res.status(400).json({ error: error.message });
     }
+    console.log("logged in");
+    const token = constructJwtToken(user);
+    res.status(200).json({ token: token });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error: error.message });
   }
+};
 
-  //Function to construct JWT TOKEN
+//Function to construct JWT TOKEN
 function constructJwtToken(user) {
-    const token = jwt.sign(
-      {
-        userId: user._id,
-        email: user.email,
-      },
-      process.env.SECRET,
-      {
-        expiresIn: "1h",
-      }
-    )
-    return token
-  }
+  const token = jwt.sign(
+    {
+      userId: user._id,
+      email: user.email,
+    },
+    process.env.SECRET,
+    {
+      expiresIn: "1h",
+    }
+  );
+  return token;
+}
 
-  module.exports = {   
-    createUser,
-  };
+module.exports = {
+  createUser,
+};
