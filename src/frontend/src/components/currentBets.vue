e910b4c061d1a655e795792246a82a77827686fd
PavanBashetty <pavankumar.rb25@gmail.com>
2023-06-08 23:17:14 +0200

    template to display placed bets

--- src/frontend/src/components/currentBets.vue
+++ src/frontend/src/components/currentBets.vue
@@ -1,15 +1,23 @@
 <template>
-
-</template>
-
-<script lang="ts">
-import {defineComponent} from 'vue';
-
-
-export default defineComponent({
-    name: 'currentBetsComp'
-})
-</script>
-
-<style>
-</style>
+    <userHeader subHeaderName="Bets Placed" />
+    </template>
+    
+    <script lang="ts">
+    import {defineComponent} from 'vue';
+    import userHeader from './userHeader.vue'
+    
+    export default defineComponent({
+        name: 'currentBetsComp',
+        data() {
+            return {
+            }
+        },
+        components: {
+            userHeader
+        }
+    })
+    </script>
+    
+    <style>
+    </style>
+    
