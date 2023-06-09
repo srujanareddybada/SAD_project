069c91f86f34ef7fa8bb463095d3a9bef1a91dc1
skandswami <59348205+skandswami@users.noreply.github.com>
2023-06-09 11:36:29 +0200

    healthcheck api added

--- src/backend/api/routes/index.js
+++ src/backend/api/routes/index.js
@@ -10,19 +10,37 @@ router.get("/", function (req, res, next) {
 /**
  * @swagger
  * /api/matches:
- *   get:
+ *   put:
+ *     tags:
+ *      - Matches
  *     summary: Get all betting events
  *     description: Retrieve a list of all events
  *     responses:
  *       200:
  *         description: A list of bets
- *       
+ *
  */
 
 router.get("/api/matches", async function (req, res, next) {
   mongodb = req.app.get("mongodb");
   await provisoner.UpcomingMatches10Days(mongodb);
   res.status(200).json("Success");
+});
+
+/**
+ * @swagger
+ * /api/healthcheck:
+ *   get:
+ *     tags:
+ *      - Healthcheck
+ *     summary:
+ *     description: Responds if the app is up and running
+ *     responses:
+ *       200:
+ *         description: A list of bets
+ */
+router.get("/api/healthcheck", function (req, res, next) {
+  res.sendStatus(200);
 });
 
 module.exports = router;
