d533219e8499d23bb093a099ac90b852c09f823a
PavanBashetty <pavankumar.rb25@gmail.com>
2023-06-11 02:34:19 +0200

    admin detail page is complete with http req

--- src/frontend/src/components/adminDetailsPage.vue
+++ src/frontend/src/components/adminDetailsPage.vue
@@ -1,18 +1,57 @@
 <template>
-    <adminHeaderComp subHeaderName="Personal Info"/>
-    <h2>Admin Detail Page</h2>
+<adminHeaderComp subHeaderName="Personal Info" />
+<div class="w-1/2 grid grid-cols-2 gap-1" style="grid-auto-rows: minmax(10px, auto); grid-auto-columns: max-content;">
+    <div class="userDetailsHeader">Full Name:</div>
+    <div class="block w-full border p-2">{{ fullName }}</div>
+    <div class="userDetailsHeader">Email:</div>
+    <div class="border p-2">{{ email }}</div>
+    <div class="userDetailsHeader">Date Of Birth:</div>
+    <div class="border p-2">{{ dob }}</div>
+    <div class="userDetailsHeader">Total Active Users: </div>
+    <div class="border p-2 bg-green-100">{{ activeUserCount }}</div>
+    <div class="userDetailsHeader">Total Blocked Users: </div>
+    <div class="border p-2 bg-red-100">{{ blockedUserCount }}</div>
+</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 import adminHeaderComp from './adminHeader.vue'
+import axios from 'axios';
 export default defineComponent({
-    name:'adminDetailsComp',
-    components:{
+    name: 'adminDetailsComp',
+    data() {
+        return {
+            fullName: ''as string,
+            email: ''as string,
+            dob: ''as string,
+            activeUserCount: 0 as number,
+            blockedUserCount: 0 as number
+        }
+    },
+    components: {
         adminHeaderComp
+    },
+    methods:{
+        async loadAdminDetails(){
+            await axios.get("/api/userdetails/" + this.email)
+            .then((res)=>{
+                console.log(res);
+            })
+            .catch((err)=>{
+                console.error(err)
+            })
+        }
+    },
+    mounted() {
+        //Get email either from  localstorage or vuex
+        //this.loadAdminDetails();
     }
 })
 </script>
 
-<style>
+<style scoped>
+.userDetailsHeader {
+    @apply block w-full font-bold border p-2 bg-gray-300 text-gray-700
+}
 </style>
--- src/frontend/src/components/adminLandingPage.vue
+++ src/frontend/src/components/adminLandingPage.vue
@@ -1,24 +1,24 @@
 <template>
-    <adminHeaderComp subHeaderName=""/>
-<h2>Admin Landing Page</h2>
+<adminHeaderComp subHeaderName="" />
+<p>Admin Landing Page</p>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
 import adminHeaderComp from './adminHeader.vue'
 export default defineComponent({
-name:'adminLandingComp',
-data(){
-    return{
-        
-    }
-},
-components:{
-    adminHeaderComp
-},
-methods:{
+    name: 'adminLandingComp',
+    data() {
+        return {
 
-}
+        }
+    },
+    components: {
+        adminHeaderComp
+    },
+    methods: {
+
+    }
 })
 </script>
 
