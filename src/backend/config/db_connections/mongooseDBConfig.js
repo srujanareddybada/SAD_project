01eb81725b454e88160897fe8615b0ada89a8d83
srujanareddybada <srujanareddy.bada@gmail.com>
2023-06-11 16:06:13 +0200

    mongoose database issue resolved

--- src/backend/config/db_connections/mongooseDBConfig.js
+++ src/backend/config/db_connections/mongooseDBConfig.js
@@ -3,13 +3,14 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 // MongoDB Atlas
-const atlasURL = `mongodb+srv://${process.env.MONGOCLOUD_USERNAME}:${process.env.MONGOCLOUD_PASSWORD}@${process.env.MONGOCLOUD_CLUSTERNAME}/${process.env.MONGOCLOUD_SPORTS_DATA_DATABASE}?retryWrites=true&w=majority`;
+const atlasURL = `mongodb+srv://${process.env.MONGOCLOUD_USERNAME}:${process.env.MONGOCLOUD_PASSWORD}@${process.env.MONGOCLOUD_CLUSTERNAME}/SportsData?retryWrites=true&w=majority`;
 var mongoConnection;
 //connect to db with mongoose
 const connectDB = async () => {
   try {
     mongoConnection = await mongoose.connect(atlasURL);
     console.log("Connected to MongoDB Atlas via mongoose");
+    console.log(atlasURL);
   } catch (error) {
     console.log(error);
   }
