6f64fbc06055354d5aa3ace8a334f923ea13fa72
skandswami <59348205+skandswami@users.noreply.github.com>
2023-06-11 08:58:38 +0200

    login refresh token apis working

--- src/backend/api/controllers/loginController.js
+++ src/backend/api/controllers/loginController.js
@@ -13,9 +13,13 @@ const loginAsync = async (req, res, next) => {
       });
 
       // Generate session token
-      const sessionToken = jwt.sign({ userId: user._id }, process.env.SECRET, {
-        expiresIn: "15m",
-      });
+      const sessionToken = jwt.sign(
+        { userId: user._id, admin: user.admin },
+        process.env.SECRET,
+        {
+          expiresIn: "15m",
+        }
+      );
       res.status(200).json({
         user: user,
         refreshToken: refreshToken,
@@ -32,8 +36,36 @@ const loginAsync = async (req, res, next) => {
   }
 };
 
+const refreshAsync = async (req, res, next) => {
+  const { refreshToken } = req.body;
+
+  jwt.verify(refreshToken, process.env.SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid refresh token." });
+    }
+
+    const userId = decoded.userId;
+    const admin = decoded.admin;
+
+    const user = { userId: userId, admin: admin };
+    // Generate a new session token
+    const sessionToken = jwt.sign(user, process.env.SECRET, {
+      expiresIn: "15m",
+    });
+
+    // Generate a new refresh token
+    const refreshToken = jwt.sign(user, process.env.SECRET, {
+      expiresIn: "15m",
+    });
+
+    // Return the new session token
+    res.json({ sessionToken: sessionToken, refreshToken: refreshToken });
+  });
+};
+
 const loginController = {
   loginAsync,
+  refreshAsync,
 };
 
 module.exports = loginController;
--- src/backend/api/routes/login.js
+++ src/backend/api/routes/login.js
@@ -24,4 +24,46 @@ var router = express.Router();
  */
 router.post("/", controller.loginAsync);
 
+/**
+ * @swagger
+ * /api/login/refresh:
+ *   post:
+ *     tags:
+ *      - Auth
+ *     summary: Refresh session token
+ *     description: Exchange a valid refresh token for a new session token.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               refreshToken:
+ *                 type: string
+ *             required:
+ *               - refreshToken
+ *     responses:
+ *       200:
+ *         description: New session token generated successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 sessionToken:
+ *                   type: string
+ *       403:
+ *         description: Invalid refresh token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ */
+
+router.post("/refresh", controller.refreshAsync);
+
 module.exports = router;
