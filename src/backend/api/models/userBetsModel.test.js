558828556c4a4fd230ec60d162a9a83e32dab45f
bittukun <dwivedikunal1995@gmail.com>
2023-06-12 17:15:39 +0200

    U: live changes on test file removed

--- src/backend/api/models/userBetsModel.test.js
+++ src/backend/api/models/userBetsModel.test.js
@@ -1,20 +1,16 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const mongoose = require('mongoose');
 const UserBetsModel = require('./userBetsModel');
-const { connectDB } = require("../../config/db_connections/mongooseDBConfig");
+
+jest.mock('./userBetsModel', () => {
+  const mockCreate = jest.fn();
+  return {
+    create: mockCreate,
+  };
+});
 
 describe('UserBets Model Tests', () => {
-    beforeAll(async () => {
-        await connectDB();
-      });
-
-  afterAll(async () => {
-    await mongoose.connection.close();
-  });
-
-  beforeEach(async () => {
-    await UserBetsModel.deleteMany({});
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should create a new user bet', async () => {
@@ -34,22 +30,29 @@ describe('UserBets Model Tests', () => {
       ],
     };
 
+    const mockCreate = UserBetsModel.create;
+    mockCreate.mockResolvedValue(userBetData);
+
     const userBet = await UserBetsModel.create(userBetData);
 
-    expect(userBet.userId).toBe(userBetData.userId);
-    expect(userBet.betAmount).toBe(userBetData.betAmount);
-    // expect(userBet.successBetReturnAmount).toBe(userBetData.successBetReturnAmount);
-    expect(userBet.betEvent).toHaveLength(userBetData.betEvent.length);
-    expect(userBet.betEvent[0].odds).toBe(userBetData.betEvent[0].odds);
-    expect(userBet.betEvent[0].eventName).toBe(userBetData.betEvent[0].eventName);
-    expect(userBet.betEvent[1].odds).toBe(userBetData.betEvent[1].odds);
-    expect(userBet.betEvent[1].eventName).toBe(userBetData.betEvent[1].eventName);
+    expect(mockCreate).toHaveBeenCalledWith(userBetData);
+    expect(userBet).toEqual(userBetData);
   });
 
   it('should require all required fields', async () => {
+    const mockError = new mongoose.Error.ValidationError();
+    mockError.errors = {
+      userId: new mongoose.Error.ValidatorError({ message: 'Path `userId` is required.' }),
+      betAmount: new mongoose.Error.ValidatorError({ message: 'Path `betAmount` is required.' }),
+    };
+
+    const mockCreate = UserBetsModel.create;
+    mockCreate.mockRejectedValue(mockError);
+
     try {
       await UserBetsModel.create({});
     } catch (error) {
+      expect(mockCreate).toHaveBeenCalledWith({});
       expect(error).toBeInstanceOf(mongoose.Error.ValidationError);
       expect(error.errors).toHaveProperty('userId');
       expect(error.errors).toHaveProperty('betAmount');
